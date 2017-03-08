23e1e3f04182f494c5129514fa0f04ed8e64160b
Alex Lagerman <lagerman.alex@gmail.com>
2017-03-08 15:52:33 -0600

    Add verify_jwt_token to create recipe action

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,7 +1,11 @@
 class Api::RecipesController < ApplicationController
+  before_action :verify_jwt_token, only: :create
   respond_to :json
 
   def index
     render json: Recipe.all
+  end
+
+  def create
   end
 end
